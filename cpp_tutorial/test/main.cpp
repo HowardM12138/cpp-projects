f7b4275c568c570de3ff14858eff9d7ac34ea791
HowardM12138 <mengbingxu@gmail.com>
2019-06-18 11:51:44 -0700

    Backup

---
+++ cpp_tutorial/test/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+
+    std::cout << "You are gay.\n\n";
+    std::cout << "Your computer is HACKED.\n\n"
+        "WARNING: SUSPICIOUS PROGRAM IS EXECUTING\n"
+        "WARNING: SUSPICIOUS PROGRAM IS EXECUTING\n"
+        "WARNING: SUSPICIOUS PROGRAM IS EXECUTING\n"
+        "WARNING: SUSPICIOUS PROGRAM IS EXECUTING\n"
+        "WARNING: SUSPICIOUS PROGRAM IS EXECUTING\n\n";
+    std::cout << "Just kidding. I don't have that ability yet.\n";
+    std::cout << "Or do I?\n";
+
+    std::string input;
+    std::cin >> input;
+
+    return 0;
+}
